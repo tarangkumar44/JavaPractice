5a36c83ab2514655a5309cd804f123922f9ef402
tarangkumar44 <tarangkumar44@gmail.com>
2025-09-01 09:56:44 +0530

    Practiced LambdaWithParameterPrinterDemo

--- LambdaExpression/LambdaWithParameterPrinter/LambdaWithParameterPrinterDemo.java
+++ LambdaExpression/LambdaWithParameterPrinter/LambdaWithParameterPrinterDemo.java
@@ -18,7 +18,7 @@ public class LambdaWithParameterPrinterDemo {
         String message1 = "Hello. My name is Tarang";
         String message2 = "I am a student";
         String message3 = "I will get a job by the end of this year";
-        MessagePrinter messagePrinter = message -> System.out.println(message);
+        MessagePrinter messagePrinter = (message) -> System.out.println(message);     // Since there’s only one parameter, parentheses are optional.
         messagePrinter.printMessage(message1);
         messagePrinter.printMessage(message2);
         messagePrinter.printMessage(message3);
